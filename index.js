a39a50dbc053afc140f43bf4f3f9fbb274b65e9e
“Gurbaj <singh.gurbaj5124871@gmail.com>
2020-12-10 13:16:46 +0530

    changes to node client

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const hosts =
   "redis-node-0,redis-node-1,redis-node-2,redis-node-3,redis-node-4,redis-node-5,redis-node-6,redis-node-7,redis-node-8";
 const password = "waitroom";
 
+console.log(
+  `inside index js: ${JSON.stringify(
+    hosts.split(",").map((host) => ({ host: `redis://${host}:6379` })),
+    null,
+    2
+  )}`
+);
+
 const redis = new Redis.Cluster(
   hosts.split(",").map((host) => ({ host: `redis://${host}:6379` })),
   {
