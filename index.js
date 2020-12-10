f1db80ccb3fc4ef2af8761c0eb14fbaaa62e843f
“Gurbaj <singh.gurbaj5124871@gmail.com>
2020-12-10 12:56:58 +0530

    changes after testing

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const Redis = require("ioredis");
 
 const hosts =
   "redis-node-0,redis-node-1,redis-node-2,redis-node-3,redis-node-4,redis-node-5,redis-node-6,redis-node-7,redis-node-8";
+const password = "waitroom";
 
 const redis = new Redis.Cluster(
   hosts.split(",").map((host) => ({ host: `redis://${host}:6379` })),
   {
     redisOptions: {
       db: 0,
+      password,
     },
   }
 );
